9e45b99452a8e0f16b85d8e8243e7d2a4da990af
Daniel Romooluwa <adeniyidaniel3@gmail.com>
2023-03-01 19:41:00 +0000

    fixed xray liddleware error

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,11 +36,12 @@ tracer = trace.get_tracer(__name__)
 #AWS X-Ray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 #---------------------------------------------
 app = Flask(__name__)
 
+XRayMiddleware(app, xray_recorder)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
