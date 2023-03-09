702158f3ddd880ee269940d488fe2d23c1ed054f
Daniel Romooluwa <adeniyidaniel3@gmail.com>
2023-03-09 15:56:09 +0000

    Cognito sign up and confirmation fixed

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -77,8 +77,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -124,6 +124,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+    request.headers.get('Authorization')
   data = HomeActivities.run()
   return data, 200
 
