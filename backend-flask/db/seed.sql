8f674aa0d0b5f5182a97b5b019083e2aa69e1c52
Daniel Romooluwa <adeniyidaniel3@gmail.com>
2023-03-25 15:14:06 +0000

    utility scripts created to work with DynamoDB

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@
 INSERT INTO public.users (display_name, handle,email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'andrew@brown.com','MOCK'),
-  ('Adeniyi Daniel', 'danny' ,'danny@tommy.com','MOCK');
+  ('Andrew Bayko', 'bayko' ,'danny@tommy.com','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
